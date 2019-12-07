fd3f41d1d5700b52e22b56c1c0e75ecd88b8b7a2
lz0305 <947564164@qq.com>
2019-12-07 09:56:46 +0800

    three

--- src/store/vuex.config.js
+++ src/store/vuex.config.js
@@ -1,6 +1,6 @@
 export default {
     state: {
-        totalPrice: 0.1
+        totalPrice: 0
     },
     mutations: {//Mutation 必须是同步函数
         increment ( state, price) {
@@ -10,13 +10,15 @@ export default {
             state.totalPrice -= price
         }
     },
-    actions: {//dispatch 分发
+    actions: {//dispatch 分发  actions 异步
         add ( context, price) {
             console.log( context );
             context.commit("decrement", price)
         }
     },
-    getters: {//getter 的返回值会根据它的依赖被缓存起来，且只有当它的依赖值发生了改变才会被重新计算。类似于中介
+    getters: {
+        //getter 的返回值会根据它的依赖被缓存起来，且只有当它的依赖值发生了改变才会被重新计算。
+        //类似于中介
         getPrice ( state ) {
             return state.totalPrice
         }
